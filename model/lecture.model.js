00771ad646aedbcc8761245b13dac13d8aff5fff
abir <antorabir@gmail.com>
2024-11-21 14:51:14 +0600

    add: lecture model

---
+++ model/lecture.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const lectureSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    duration: { type: String },
+    description: { type: String },
+    videoUrl: { type: String },
+    position: { type: Number },
+
+    subtitle: { type: String },
+    thumbnailImage: { type: String },
+    welcomeMessage: { type: String },
+    congratulationsMessage: { type: String },
+    isDisabled: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
+
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+    section: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Section",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Lecture", lectureSchema);
